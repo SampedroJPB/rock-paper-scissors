bd167d3c16d12eba28f10917201036c14ca00a13
Sampedro James Paul Bagtas <72242960+SampedroJPB@users.noreply.github.com>
2026-02-25 19:33:50 +0900

    added round function and finished the project
    added function playRound and its logic to play 5 rounds

--- script.js
+++ script.js
@@ -14,22 +14,50 @@ function getComputerChoice () {
     }
 }
 
-function playGame (humanChoice, computerChoice) {
-    if (humanChoice === "rock" && computerChoice === "paper") {
-        return console.log("YOU LOSE.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
-    } else if (humanChoice === "rock" && computerChoice === "scissors") {
-        return console.log("You win.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
-    } else if (humanChoice === "scissors" && computerChoice === "rock") {
-        return console.log("You lose.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
-    } else if (humanChoice === "scissors" && computerChoice === "paper") {
-        return console.log("You win.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
-    } else if (humanChoice === "paper" && computerChoice === "scissors") {
-        return console.log("You lose.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
-    } else if (humanChoice === "paper" && computerChoice === "rock") {
-        return console.log("You win.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
-    } else {
-        return console.log("Draw.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
+function playRound (round) {
+
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playGame (humanChoice, computerChoice) {
+        if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log("YOU LOSE.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
+            computerScore++;
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            console.log("You win.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
+            humanScore++;
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            console.log("You lose.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
+            computerScore++;
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            console.log("You win.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
+            humanScore++;
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            console.log("You lose.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
+            computerScore++;
+        } else if (humanChoice === "paper" && computerChoice === "rock") {
+            console.log("You win.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
+            humanScore++;
+        } else {
+            console.log("Draw.\nYou picked " + humanChoice + " and computer picked " + computerChoice);
+        }
     }
+
+    for (let i = 0; i < round; i++) {
+        playGame(getHumanChoice(), getComputerChoice());
+        // Another method:
+        // const humanSelection = getHumanChoice();
+        // const computerSelection = getComputerChoice();
+        // playGame(humanSelection, computerSelection);
+    }
+
+    if (humanScore > computerScore) {
+        console.log("You win.\nYour score is: " + humanScore + "\nComputer score is: " + computerScore);
+    } else if (humanScore < computerScore) {
+        console.log("You lose.\nYour score is: " + humanScore + "\nComputer score is: " + computerScore);
+    } else {
+        console.log("Draw.\nBoth you and computer had the same score.");
+    }  
 }
 
-playGame(getHumanChoice(), getComputerChoice());
+playRound(5);
